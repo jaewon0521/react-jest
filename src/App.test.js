5b10c8d88f9065bca92d57a8d9995c2db308c027
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-10-25 21:12:18 +0900

    Test and code function to transform camel case colors to spaced colors

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import App from "./App";
+import App, { replaceCamelWithSpaces } from "./App";
 
 test("button has correct initial color", () => {
   render(<App />);
@@ -56,4 +56,18 @@ test("Clicked disabled button has gray background and reverts to blue", () => {
 
   fireEvent.click(button);
   expect(button).toHaveStyle({ backgroundColor: "blue" });
+});
+
+describe("spaces before camel-case capital letters", () => {
+  test("Works for no inner capital letters", () => {
+    expect(replaceCamelWithSpaces("Red")).toBe("Red");
+  });
+
+  test("Works for one inner capital letter", () => {
+    expect(replaceCamelWithSpaces("MidnightBlue")).toBe("Midnight Blue");
+  });
+
+  test("Works for multiple inner capital letters", () => {
+    expect(replaceCamelWithSpaces("MediumVioletRed")).toBe("Medium Violet Red");
+  });
 });
