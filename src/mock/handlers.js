3f5c89b807c0f03d9b412612b927b70c30555006
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-10-30 11:01:43 +0900

    toppingoption component add And test

--- src/mock/handlers.js
+++ src/mock/handlers.js
@@ -9,4 +9,13 @@ export const handlers = [
       ])
     );
   }),
+  rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "Cherries", imagePath: "/images/cherries.png" },
+        { name: "M&Ms", imagepath: "/images/m-and-ms.png" },
+        { name: "Hot fudge", imagepath: "/images/hot-fudge.png" },
+      ])
+    );
+  }),
 ];
--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Row } from "react-bootstrap";
 import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
@@ -17,7 +18,7 @@ const Options = ({ optionType }) => {
   }, [optionType]);
 
   // TODO: replace 'null' with Topping option when available
-  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
     <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />
